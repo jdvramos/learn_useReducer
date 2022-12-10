4985774450531191d1228d688b2b9c27f974ad01
jdvramos <jonndenzelramos@gmail.com>
2022-12-10 16:12:42 +0800

    useReducer (simple state & action)

---
+++ src/CounterOne.jsx
@@ -0,0 +1,30 @@
+import React, { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "increment":
+      return currentState + 1;
+    case "decrement":
+      return currentState > 0 ? currentState - 1 : 0;
+    case "reset":
+      return initialState;
+    default:
+      return currentState;
+  }
+};
+
+const CounterOne = () => {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <div>Count - {count}</div>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
+    </div>
+  );
+};
+
+export default CounterOne;
