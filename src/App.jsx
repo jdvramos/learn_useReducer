d28a42f5763ad297668f1c94e0380c2e80c58aa0
jdvramos <jonndenzelramos@gmail.com>
2022-12-10 16:46:40 +0800

    useReducer (complex state & action)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import CounterOne from "./CounterOne";
+import CounterTwo from "./CounterTwo";
 
 function App() {
   return (
     <div className="App">
-      <CounterOne />
+      {/* <CounterOne /> */}
+      <CounterTwo />
     </div>
   );
 }
